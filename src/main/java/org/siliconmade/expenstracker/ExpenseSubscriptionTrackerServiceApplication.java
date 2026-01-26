51ad7418dc1d210af913c54bbd5c9ed58ffe4d42
baris.karapelit <bkarapelit48@gmail.com>
2026-01-26 23:41:25 +0300

    Initial commit

---
+++ src/main/java/org/siliconmade/expenstracker/ExpenseSubscriptionTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.siliconmade.expenstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSubscriptionTrackerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseSubscriptionTrackerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/siliconmade/expenstracker/ExpenseSubscriptionTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.siliconmade.expenstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSubscriptionTrackerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
